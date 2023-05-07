26395ce69ada601e84ae6bad6e056d3a5f6c3a80
NikolasSerg <nikolasalon@gmail.com>
2023-05-07 18:43:47 +0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+function foo() {
+    console.log("test");
+    let x = 2;
+    x++;
+    const y = x > 10 ? 15 : 5;
+    return x + y;
+}
+
+let test = 'Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне.';
+
+foo();
